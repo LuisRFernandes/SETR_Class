0222980fc1b6df0d8e2583655b1ca3ae101dbb32
Luis Fernandes <luiscfernandes1997@gmail.com>
2024-03-10 16:49:29 +0000

    Initial version

---
+++ Assigment1/dll.h
@@ -0,0 +1,34 @@
+#include <stdint.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <stdbool.h>
+
+// Node - estrutura
+typedef struct DLL_Node {
+    uint16_t key;           // Chave - uint16
+    unsigned char *data;    // Dados - unsigned char
+    struct DLL_Node *previous;
+    struct DLL_Node *next;
+
+}DLL_Node;
+
+//DLL - estrutura
+typedef struct DLL{
+
+
+}DLL;
+
+// Headers das funções
+void MyDLLInit(DLL *My_dll, uint8_t max_elements, );
+
+bool MyDLLInsert();
+
+bool MyDLLRemove();
+
+unsigned char *MyDLLFind();
+
+unsigned char *MyDLLFind_Next();
+
+unsigned char *MyDLLFind_Previous();
+
+
